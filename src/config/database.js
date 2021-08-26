42e496cf6ef2f968cb0a045460023a6ed0c09027
Fernando <fernando.vgarcia@hotmail.com>
2021-08-26 13:51:31 -0300

    --

--- src/config/database.js
+++ src/config/database.js
@@ -3,12 +3,8 @@ require("dotenv").config({
 });
 
 module.exports = {
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  dialect: process.env.DB_DIALECT || "postgres",
-  storage: "./__tests__/database.sqlite",
+  dialect: "sqlite",
+  storage: process.env.DB_DB_PATH || "./database.sqlite",
   operatorsAliases: false,
   logging: false,
   define: {
